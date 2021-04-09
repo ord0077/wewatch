587a54de29b31d122b0051e36389c74e9fc54ed3
ord0077 <ord0077@gmail.com>
2021-04-09 19:29:55 +0500

    tttt

--- app/Http/Controllers/DailyHseReportController.php
+++ app/Http/Controllers/DailyHseReportController.php
@@ -49,82 +49,97 @@ class DailyHseReportController extends Controller
                     return  ['success' => false, 'error' =>  $validator->errors()];
             }
 
-            $hsefields = array(
+  
 
-                'project_id' => $request->project_id,
-                'user_id' => $request->user_id,
-                'date' => $request->date,
-                'description_confidential' => $request->description_confidential,
-                'daily_situation_summary' => $request->daily_situation_summary,
-                'project_key_meeting' => $request->project_key_meeting,
-                'toolbox_talk' => $request->toolbox_talk,
-                'procurement_request' => $request->procurement_request,
-                'red_flag' => $request->red_flag,
+            // DB::table('bulid_activities')->insert($bafields);   
+            // DB::table('project_health_compliances')->insert($phcfields);    
+            // DB::table('project_details')->insert($pfields);   
+            // DB::table('hazard_identifies')->insert($hifields);   
+            // DB::table('near_miss_reportings')->insert($nmrfields);   
+            // DB::table('covid_compliances')->insert($ccfields);   
+
+
+            DB::beginTransaction();
+
+            try {
             
-                );
 
-        $success = DHR::create($hsefields);
+                $hsefields = array(
 
-        $pfields = array(
-            'd_h_r_id' => $success->id,
-            'weather' => $request->weather,
-            'wind_strength' => $request->wind_strength,
-            'weather_wind_remarks' => $request->weather_wind_remarks,  
-            'design_build_time' => $request->design_build_time,
-            'daily_operation_man_hour' => $request->daily_operation_man_hour,
-            'design_time_hour_remarks' => $request->design_time_hour_remarks,
-            'contractors' => $request->contractors,
-            'staff_numbers' => $request->staff_numbers,
-            'shift_pattern' => $request->shift_pattern,
-            'daily_man_hours' => $request->daily_man_hours,
-            'type_contractors' => $request->type_contractors,
-            'total_man_days' => $request->total_man_days,
-            'total_man_hours' => $request->total_man_hours,
-            'total_lost_work_hours' => $request->total_lost_work_hours
-
-        );
-
-        $bafields = array(
-
-            'd_h_r_id' => $success->id,
-           'activites' => $request->activites,
-            'occurrence' => $request->occurrence,
-           'remarks' => $request->remarks
-
-
-        );
-
-        $phcfields = array(
-
-            'd_h_r_id' => $success->id,
-            'project_health_activites' => $request->project_health_activites,
-           'project_health_occurrence' =>  $request->project_health_occurrence,
-            'project_health_remarks' =>  $request->project_health_remarks
-        );
-
-        $hifields =array(
-
-           'd_h_r_id' => $success->id,
-         'hazard_identify_activites' => $request->hazard_identify_activites,
-           'hazard_identify_occurrence' => $request->hazard_identify_occurrence,
-           'hazard_identify_remarks' => $request->hazard_identify_remarks
-        );
-
-        $nmrfields =array(
-
-           'd_h_r_id' => $success->id,
-           'near_miss_activites' => $request->near_miss_activites,
-            'near_miss_occurrence' => $request->near_miss_occurrence,
-            'near_miss_remarks' => $request->near_miss_remarks
-        );
-
-        $ccfields = array(
-
-            'd_h_r_id' => $success->id,
-            'covid_compliance_activites' => $request->covid_compliance_activites,
-            'covid_compliance_occurrence' => $request->covid_compliance_occurrence, 
-            'covid_compliance_remarks' => $request->covid_compliance_remarks
-        );
+                    'project_id' => $request->project_id,
+                    'user_id' => $request->user_id,
+                    'date' => $request->date,
+                    'description_confidential' => $request->description_confidential,
+                    'daily_situation_summary' => $request->daily_situation_summary,
+                    'project_key_meeting' => $request->project_key_meeting,
+                    'toolbox_talk' => $request->toolbox_talk,
+                    'procurement_request' => $request->procurement_request,
+                    'red_flag' => $request->red_flag,
+                
+                    );
+    
+            $success = DHR::create($hsefields);
+    
+            $pfields = array(
+                'd_h_r_id' => $success->id,
+                'weather' => $request->weather,
+                'wind_strength' => $request->wind_strength,
+                'weather_wind_remarks' => $request->weather_wind_remarks,  
+                'design_build_time' => $request->design_build_time,
+                'daily_operation_man_hour' => $request->daily_operation_man_hour,
+                'design_time_hour_remarks' => $request->design_time_hour_remarks,
+                'contractors' => $request->contractors,
+                'staff_numbers' => $request->staff_numbers,
+                'shift_pattern' => $request->shift_pattern,
+                'daily_man_hours' => $request->daily_man_hours,
+                'type_contractors' => $request->type_contractors,
+                'total_man_days' => $request->total_man_days,
+                'total_man_hours' => $request->total_man_hours,
+                'total_lost_work_hours' => $request->total_lost_work_hours
+    
+            );
+    
+            $bafields = array(
+    
+                'd_h_r_id' => $success->id,
+               'activites' => $request->activites,
+                'occurrence' => $request->occurrence,
+               'remarks' => $request->remarks
+    
+    
+            );
+    
+            $phcfields = array(
+    
+                'd_h_r_id' => $success->id,
+                'project_health_activites' => $request->project_health_activites,
+               'project_health_occurrence' =>  $request->project_health_occurrence,
+                'project_health_remarks' =>  $request->project_health_remarks
+            );
+    
+            $hifields =array(
+    
+               'd_h_r_id' => $success->id,
+             'hazard_identify_activites' => $request->hazard_identify_activites,
+               'hazard_identify_occurrence' => $request->hazard_identify_occurrence,
+               'hazard_identify_remarks' => $request->hazard_identify_remarks
+            );
+    
+            $nmrfields =array(
+    
+               'd_h_r_id' => $success->id,
+               'near_miss_activites' => $request->near_miss_activites,
+                'near_miss_occurrence' => $request->near_miss_occurrence,
+                'near_miss_remarks' => $request->near_miss_remarks
+            );
+    
+            $ccfields = array(
+    
+                'd_h_r_id' => $success->id,
+                'covid_compliance_activites' => $request->covid_compliance_activites,
+                'covid_compliance_occurrence' => $request->covid_compliance_occurrence, 
+                'covid_compliance_remarks' => $request->covid_compliance_remarks
+            );
 
             DB::table('bulid_activities')->insert($bafields);   
             DB::table('project_health_compliances')->insert($phcfields);    
@@ -132,32 +147,19 @@ class DailyHseReportController extends Controller
             DB::table('hazard_identifies')->insert($hifields);   
             DB::table('near_miss_reportings')->insert($nmrfields);   
             DB::table('covid_compliances')->insert($ccfields);   
-
-
-        //     DB::beginTransaction();
-
-        //     try {
-            
-
-        //     DB::table('bulid_activities')->insert($bafields);   
-        //     DB::table('project_health_compliances')->insert($phcfields);    
-        //     DB::table('project_details')->insert($pfields);   
-        //     DB::table('hazard_identifies')->insert($hifields);   
-        //     DB::table('near_miss_reportings')->insert($nmrfields);   
-        //     DB::table('covid_compliances')->insert($ccfields);   
-        //    // $id = DB::table('d_h_r_s')->insertGetId($hsefields);   
+           // $id = DB::table('d_h_r_s')->insertGetId($hsefields);   
         
           
             list($status,$data) = $success ? [true, $success] : [false, ''];
             return ['success' => $status,'data' => $data];
 
-        //     DB::commit();
-        //     // all good
-        //     } catch (Exception $e) {
-        //       DB::rollback();
-        //     // something went wrong
-        //     return response()->json($e->errorInfo ?? 'unknown error');
-        //     }
+            DB::commit();
+            // all good
+            } catch (Exception $e) {
+              DB::rollback();
+            // something went wrong
+            return response()->json($e->errorInfo ?? 'unknown error');
+            }
 
 
     
