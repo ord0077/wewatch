e4f618b717f7d01e41079b682f545a997bc03db2
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-05-19 14:50:33 +0500

    test

--- resources/views/emails/dailysecuritypdf.blade.php
+++ resources/views/emails/dailysecuritypdf.blade.php
@@ -1,34 +1,66 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        <!-- Scripts -->
-        @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
-
-        <style>
+<html>
+<head>
+<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+	<title>Daily HSE Report</title>
+</head>
+<style type="text/css">
 
 
 
 
-  </style>
-    </head>
-    <body>
 
 
+@font-face {
+    font-family: AR;
+    src: url('./fonts/AR.otf');
+    src: local('AR'), url('./fonts/AR.otf') format('otf'), url('.') format('truetype'); */
+    /* src: url('./SansationLight.eot');
+	src: local('SansationLight'), url('./SansationLight.woff') format('woff'), url('./SansationLight.ttf') format('truetype'); */
+}
 
-            <h1>Daily Security Report</h1>
-        @inertia
-    </body>
+*{
+     font-family: AR;
+}
+
+div.main{
+    max-width:100%;
+    width:100%;
+}
+.color-red{
+    color:red;
+}
+.primary-table{
+    width:100%;
+    border:0.01px solid #717171;
+    border-spacing: 0px;
+}
+.primary-table tr th, .primary-table tr td{
+    text-align:left;
+    padding:15px;
+    border:0.01px solid #717171;
+}
+.primary-table tr th{
+    background-color: #315AA5;
+    color: white;
+    font-weight:bold;
+}
+.top-margin-20{
+    margin-top: 20px;
+}
+h3 {
+    line-height: 0.4;
+    margin-top: 30px;
+}
+</style>
+<body >
+    <div class="main">
+
+        <h1> test font </h1>
+  </div>
+</body>
 </html>
