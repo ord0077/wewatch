4100b069b4a0e0cfdca792bc59e605e345e23f25
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-05-19 13:27:18 +0500

    test

--- resources/views/emails/dailysecuritypdf.blade.php
+++ resources/views/emails/dailysecuritypdf.blade.php
@@ -22,7 +22,7 @@
 @font-face {
     font-family: AR;
     src: url('./fonts/AR.otf');
-    src: local('AR'), url('./fonts/AR.otf') format('otf'), url('.') format('truetype'); */
+    src: local('AR'), url('./fonts/AR.otf') format('otf'), url('.') format('truetype');
     /* src: url('./SansationLight.eot');
 	src: local('SansationLight'), url('./SansationLight.woff') format('woff'), url('./SansationLight.ttf') format('truetype'); */
 }
@@ -30,7 +30,7 @@
 
   </style>
     </head>
-    <body>
+    <body style="text-align:center; font-family:AR;">
 
 
 
