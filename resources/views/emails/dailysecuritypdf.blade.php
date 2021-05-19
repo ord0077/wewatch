27bb934748897082bd0c9d0b0ca1cd71d21e0c41
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-05-19 13:20:44 +0500

    test

--- resources/views/emails/dailysecuritypdf.blade.php
+++ resources/views/emails/dailysecuritypdf.blade.php
@@ -29,7 +29,50 @@
 
 div.main{
     max-width:100%;
-    width:100%;
+    width:100%;<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        @routes
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <style>
+/*
+@font-face {
+    font-family: AR;
+    src: url('./fonts/AR.otf');
+    src: local('AR'), url('./fonts/AR.otf') format('otf'), url('./fonts/AR.otf') format('truetype');
+    src: url('./SansationLight.eot');
+	src: local('SansationLight'), url('./SansationLight.woff') format('woff'), url('./SansationLight.ttf') format('truetype');
+}
+ */
+ @font-face {
+    font-family: AR;
+    src: url('{{ public_path('/fonts/AR.otf') }}');
+}
+  </style>
+    </head>
+    <body style="text-align:center; font-family:AR;">
+
+
+
+            <h1 style="text-align:center; font-family:AR;">Daily Security Report</h1>
+        @inertia
+    </body>
+</html>
+
 }
 .color-red{
     color:red;
