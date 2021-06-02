49f7fea111e4a0afaffe26ee5bde30137f0f3a30
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-06-02 15:29:26 +0500

    title change reports

--- resources/views/emails/dailysecuritypdf.blade.php
+++ resources/views/emails/dailysecuritypdf.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-	<title>Daily HSE Report</title>
+	<title>Daily Security Report</title>
 </head>
 <style type="text/css">
 
